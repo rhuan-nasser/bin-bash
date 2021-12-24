88f64dc54358c2b0cbfafe5db826d9f88564227c
rhuan-nasser <66943565+rhuan-nasser@users.noreply.github.com>
2021-12-24 13:33:55 -0300

    modificação
    modificação

--- monitoramento_de_memoria.sh
+++ monitoramento_de_memoria.sh
@@ -4,31 +4,34 @@ verifica_memoria_disponivel(){
 memoria_disponivel=$(free -m | awk '{ print $7  }' | grep [0-9])
 }
 
+verifica_memoria_disponivel
+
 while :
 do
 
-	while [ $memoria_disponivel -le 2000 ] 
-	do
-		verifica_memoria_disponivel
+        while [ $memoria_disponivel -le 2000 ] 
+        do
+                verifica_memoria_disponivel
 
-		mail -a "Content-Type: text/plain; charset=UTF-8" -s "Consumo acima de 6GB de RAM" exemplo@servidor.com<<msg
+                mail -a "Content-Type: text/plain; charset=UTF-8" -s "Consumo acima de 6GB de RAM" rhuan.nasser@iti.gov.br<<msg
 Prezados,
 
     Segue abaixo informações mais detalhadas do consumo de memória
 no servidor xpto.
 
-$(ps -eo pid,%mem,cmd --sort=-%mem | head -11 | cut -d- -f1)
+$(for processo in $(ps -e -o pid --sort -size | head -11); do ps -p $processo -o pid,comm= | grep -v PID; done 2>/dev/null)
+
 msg
 
 verifica_memoria_disponivel
-       
-	while [ $memoria_disponivel -le 2300  ]
-	do
-		verifica_memoria_disponivel
-		sleep 60
-	done
 
-	done
+        while [ $memoria_disponivel -le 2300  ]
+        do
+                verifica_memoria_disponivel
+                sleep 60
+        done
+
+        done
 
 verifica_memoria_disponivel
 
